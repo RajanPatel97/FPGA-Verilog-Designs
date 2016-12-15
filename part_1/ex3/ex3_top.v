50e37fe5856d8e5b7824130e54696c1d4939dc17
RajanPatel97 <rajan.n.patel.97@gmail.com>
2016-12-15 16:30:41 +0000

    Update ex3_top.v

--- part_1/ex3/ex3_top.v
+++ part_1/ex3/ex3_top.v
@@ -4,13 +4,13 @@ module ex3_top (
 		HEX1,
 		HEX2
 );		
-		input		[9:0] SW;
+		input	[9:0] SW;	//define inputs and outputs	
 		output 	[6:0] HEX0;
 		output 	[6:0] HEX1;
 		output 	[6:0] HEX2;
 		
-		hex_to_7seg		SEG0 (HEX0, SW[3:0]);
-		hex_to_7seg		SEG1 (HEX1, SW[7:4]);
+		hex_to_7seg		SEG0 (HEX0, SW[3:0]);	//define  module behaviour
+		hex_to_7seg		SEG1 (HEX1, SW[7:4]);	
 		hex_to_7seg		SEG2 (HEX2, SW[9:8]);
 		
 endmodule 
