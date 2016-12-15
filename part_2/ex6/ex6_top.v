2968613009d9368a032a5e5ab655bb3b03c2a0cf
RajanPatel97 <rajan.n.patel.97@gmail.com>
2016-12-15 17:32:14 +0000

    Update ex6_top.v

--- part_2/ex6/ex6_top.v
+++ part_2/ex6/ex6_top.v
@@ -7,16 +7,16 @@ module ex6_top (
 	HEX3,
 	HEX4
 );
-		input		[1:0] KEY;
+		input	[1:0] KEY;
 		input 	CLOCK_50;
 		output 	[6:0]	HEX0, HEX1, HEX2, HEX3, HEX4;
 		
 		
 		wire [15:0]	B;		// binary input number
 		wire [3:0]	BCD_0, BCD_1, BCD_2, BCD_3, BCD_4;   // BCD digit LSD to MSD
-		wire tick;
+		wire 		tick;
 		
-		clk_tick			DIVIDE (CLOCK_50, count, tick);
+		clk_tick		DIVIDE (CLOCK_50, count, tick);
 		counter_16		COUNTER (CLOCK_50, ~KEY[0] && tick, B, ~KEY[1]);
 		bin2bcd_16		CONVERTER (B, BCD_0, BCD_1, BCD_2, BCD_3, BCD_4);
 		hex_to_7seg		SEG0 (HEX0, BCD_0);
